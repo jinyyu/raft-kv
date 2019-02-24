62dba4784e04b458a488f0c3759573c05357cbed
ljy <jinyyu@hotmail.com>
2019-02-24 17:32:12 +0800

    add test

--- tests/test_raft2.cpp
+++ tests/test_raft2.cpp
@@ -6,6 +6,33 @@
 
 using namespace kvd;
 
+TEST(raft, SingleNodeCandidate)
+{
+    std::vector<RaftPtr> peers{nullptr};
+    Network tt(peers);
+    {
+        proto::MessagePtr msg(new proto::Message());
+        msg->from = 1;
+        msg->to = 1;
+        msg->type = proto::MsgHup;
+        tt.send(msg);
+    }
+    ASSERT_TRUE(tt.peers[1]->state_ == RaftState::Leader);
+}
+
+TEST(raft, SingleNodePreCandidate)
+{
+    std::vector<RaftPtr> peers{nullptr};
+    Network tt(preVoteConfig, peers);
+    {
+        proto::MessagePtr msg(new proto::Message());
+        msg->from = 1;
+        msg->to = 1;
+        msg->type = proto::MsgHup;
+        tt.send(msg);
+    }
+    ASSERT_TRUE(tt.peers[1]->state_ == RaftState::Leader);
+}
 
 int main(int argc, char* argv[])
 {
